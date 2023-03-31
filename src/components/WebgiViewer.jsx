991d265ccb44a45f1e3701dfd4b64cbc752c87fd
anushkadeshpande <anushkaa5000@gmail.com>
2023-04-01 02:52:42 +0530

    added 3d model

--- src/components/WebgiViewer.jsx
+++ src/components/WebgiViewer.jsx
@@ -1,14 +1,63 @@
-import {useRef} from 'react'
+import {
+  useRef,
+  useState,
+  useCallback,
+  forwardRef,
+  useImperativeHandle,
+  useEffect,
+} from "react";
+import {
+  ViewerApp,
+  AssetManagerPlugin,
+  GBufferPlugin,
+  ProgressivePlugin,
+  TonemapPlugin,
+  SSRPlugin,
+  SSAOPlugin,
+  BloomPlugin,
+  GammaCorrectionPlugin,
+  addBasePlugins,
+  TweakpaneUiPlugin,
+  AssetManagerBasicPopupPlugin,
+  CanvasSnipperPlugin,
+  mobileAndTabletCheck,
+} from "webgi";
+
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 const WebgiViewer = () => {
+  const canvasRef = useRef(null);
 
-    const canvasRef = useRef(null)
-    
+  // to cache and won't be re rendered everytime
+  const setupViewer = useCallback(async () => {
+    // Initialize the viewer
+    const viewer = new ViewerApp({
+      canvas: canvasRef.current,
+    });
+
+    // Add some plugins
+    const manager = await viewer.addPlugin(AssetManagerPlugin);
+
+    // or use this to add all main ones at once.
+    await addBasePlugins(viewer);
+
+    // Add more plugins not available in base, like CanvasSnipperPlugin which has helpers to download an image of the canvas.
+    await viewer.addPlugin(CanvasSnipperPlugin);
+
+    // This must be called once after all plugins are added.
+    viewer.renderer.refreshPipeline();
+
+    await manager.addFromPath("scene-black.glb");
+  }, []);
+
+  useEffect(() => setupViewer(), [])
+
   return (
     <div id="webgi-canvas-container">
-        <canvas id="webgi-canvas" ref={canvasRef} />
+      <canvas id="webgi-canvas" ref={canvasRef} />
     </div>
-  )
-}
+  );
+};
 
-export default WebgiViewer
+export default WebgiViewer;
