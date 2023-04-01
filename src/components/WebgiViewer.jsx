d4eb7a66652bd5384a9dc92e871b8ff7fe481fe1
anushkadeshpande <anushkaa5000@gmail.com>
2023-04-01 12:47:41 +0530

    feat: Added preview section

--- src/components/WebgiViewer.jsx
+++ src/components/WebgiViewer.jsx
@@ -36,9 +36,14 @@ const WebgiViewer = forwardRef((props, ref) => {
   const [targetRef, setTargetRef] = useState(null)
   const [cameraRef, setCameraRef] = useState(null)
   const [positionRef, setPositionRef] = useState(null)
+  const canvasContainerRef = useRef(null);
+  const [ previewMode, setPreviewMode ] = useState(false)
 
   useImperativeHandle(ref, () => ({
     triggerPreview() {
+      setPreviewMode(true)
+      canvasContainerRef.current.style.pointerEvents = "all"
+      props.contentRef.current.style.opacity = "0"
       gsap.to(positionRef, {
         x: 13.04,
         y: -2.01,
@@ -58,6 +63,7 @@ const WebgiViewer = forwardRef((props, ref) => {
           duration: 2
         }
       )
+      viewerRef.scene.activeCamera.setCameraOptions({ controlsEnabled: true });
     }
   }))
 
@@ -126,9 +132,49 @@ const WebgiViewer = forwardRef((props, ref) => {
 
   useEffect(() => setupViewer(), [])
 
+  const handleExit = useCallback(() => {
+    canvasContainerRef.current.style.pointerEvents = "none"
+    props.contentRef.current.style.opacity = "1"
+    viewer.scene.activeCamera.setCameraOptions({controlsEnabled: false})
+    setPreviewMode(false)
+
+    //get back to its original position
+    gsap.to(positionRef, {
+      x: 1.56,
+      y: 5.0,
+      z: 0.01,
+      scrollTrigger: {
+          trigger: '.display-section',
+          start: "top bottom",
+          end: "top top",
+          scrub: 2,
+          immediateRender: false
+      },
+      onUpdate: () => {
+        viewerRef.setDirty()
+        cameraRef.positionTargetUpdated(true)
+      }
+  });
+  gsap.to(targetRef, {
+      x: -0.55,
+      y: 0.32,
+      z: 0.0,
+      scrollTrigger: {
+          trigger: '.display-section',
+          start: "top bottom",
+          end: "top top",
+          scrub: 2,
+          immediateRender: false
+      },
+  })
+  }, [canvasContainerRef, viewerRef, positionRef, cameraRef, targetRef])
+
   return (
-    <div id="webgi-canvas-container">
+    <div ref={canvasContainerRef} id="webgi-canvas-container">
       <canvas id="webgi-canvas" ref={canvasRef} />
+      {
+        previewMode && <button className="button" onClick={handleExit}>Exit</button>
+      }
     </div>
   );
   })
